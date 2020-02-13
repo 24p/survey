f9b23d6dc5f8da50ba384b59dc2d003def7cc528
Abdallah Kawji <abdallah@styla.com>
2020-02-13 23:06:24 +0100

    fetch survey and store it in the state

--- src/views/Survery/index.tsx
+++ src/views/Survery/index.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './styles.css';
+import { get } from '../../utilities/xhr';
 
 export const SurveyPage = () => {
     const {lang, surveyId} = useParams();
+    const [survey, setSurvey] = useState<any>(null);
 
+    useEffect(() => {
+        (async () => {
+            // fetch survey with lang and surveyId
+            try {
+                const response = await get(
+                    `gds-platform.com/survey/${lang}/${surveyId}`
+                );
+                setSurvey(response);
+            }
+            catch(err) {
+                console.warn(err);
+            }
+        })();
+    }, [lang, surveyId]);
+
+    if (!survey) {
+        return <div>loading survey</div>
+    }
 
     return (
         <div className='language-selection'>
